f589e8e7421df33530f8c083b7d7d4d549b9dcca
Nick-blip-creator <63702722+Nick-blip-creator@users.noreply.github.com>
2020-04-21 21:03:13 -0700

    Add files via upload

---
+++ Basic JavaScript Project/JS/Basic_JavaScript_1.js
@@ -0,0 +1,12 @@
+window.alert("Hello");
+
+var string1 = "Have a nice day ";
+var string2= "Nick";
+var concat = string1 + string2;
+document.write(concat);
+
+document.write(6 * 6);
+
+var blue = "I am blue";
+var blue =blue.fontcolor("blue");
+document.write(blue);
