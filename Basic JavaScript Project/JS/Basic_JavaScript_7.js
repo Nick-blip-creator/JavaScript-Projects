e4c00157076105baa96317ea50ceeb8329f0f845
Nick-blip-creator <63702722+Nick-blip-creator@users.noreply.github.com>
2020-04-22 23:13:42 -0700

    Add files via upload

---
+++ Basic JavaScript Project/JS/Basic_JavaScript_7.js
@@ -0,0 +1,56 @@
+
+var A = 20;
+function add() {
+    var A = 20;
+    document.write(100 + A);
+}
+add(2, 5);
+
+function multiply() {
+    var x = 50;
+    var y = 100;
+    document.write(x * y);
+}
+multiply();
+
+
+function add1() {
+    console.log(A + 100);
+}
+add1(3,4);
+
+
+function get_Date() {
+    if (new Date().getHours() < 23) {
+        Greeting = "Have a good day!";
+        } else {
+            Greeting = "go to bed";
+        }
+        document.getElementById("Greeting").innerHTML = Greeting;
+    }
+
+  
+    function howHeight() {
+        height = document.getElementById("height").value;
+        if (height >= 6) {
+            stringData = "You are tall enough";
+        } else {
+            stringData = "You are too short";
+        }
+        document.getElementById("howTall").innerHTML = stringData;
+    }
+
+    function Time_function() {
+        var Time = new Date().getHours();
+        var Reply;
+        if (Time < 12 == Time > 0) {
+            Reply = "It's the mornin"
+        }
+        else if (Time > 12 == Time < 18) {
+            Reply ="It's afternoon";
+        }
+        else {
+            Reply = "It's evening time";
+        }
+        document.getElementById("Time_of_day").innerHTML = Reply;
+    }
